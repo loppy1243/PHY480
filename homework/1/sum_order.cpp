c62e3d65e3f49bee5e06258dd61875c1a0bdfd62
Nicholas Todoroff <loppy1243@gmail.com>
2019-02-08 02:32:52 -0500

    homwork/1:sum_order: Improve plot, add comparison with actual summations

--- homework/1/sum_order.cpp
+++ homework/1/sum_order.cpp
@@ -24,45 +24,49 @@ T sum_down(int n) {
 }
 
 void usage(std::ostream &stream, char const *prog_name) {
-    stream << "Usage: " << prog_name << "[-n n_max] [-s samples]" << std::endl
-           << "    Sum n terms for n in the range 1 to n_max for the given number of samples."
-              << std::endl;
+    stream << "Usage: " << prog_name << "[-n n_max] [-s scaling]" << std::endl
+           << "    Defaults: n_max=1000000, scaling=5" << std::endl
+           << std::endl
+           << "    Sum n terms for n in the range 1 to n_max. Values of n are chosen by" << std::endl
+           << "    starting with at n=1 with a step size of 1, and then scaling the step size" << std::endl
+           << "    for each power of 10. E.g.," << std::endl
+           << "          1 <= n < 10   => step size = 1" << std::endl
+           << "         10 <= n < 100  => step size = 5 if scaling=5" << std::endl
+           << "        100 <= n < 1000 => step size = 25" << std::endl;
 }
 
 int main(int argc, char **argv) {
-    int optchar, n_max=1000, samples=1000;
+    int optchar, n_max=1000000, scaling=5;
     while ((optchar = getopt(argc, argv, "n:s:")) != -1) {
         switch (optchar) {
             case 'n': n_max = std::stoi(optarg); break;
-            case 's': samples = std::stoi(optarg); break;
+            case 's': scaling = std::stoi(optarg); break;
         }
     }
-    int step = (n_max - n_max % samples) / samples;
-
     int const ndigits = 1 + (int) floor(log10(n_max));
-    // If ndigits < the width of the terms column header (7), then use that.
+    // If ndigits < the width of the terms column-header (7), then use that.
     int const terms_col_width = ndigits < 5 ? 5 : ndigits;
     int const prec = 8;
 
     char const *const space = "   ";
     // Print header "terms sum_up sum_down" with appropriate spacing.
     std::cout << std::left
-                 << std::setw(terms_col_width) << "terms"
+                 << std::setw(terms_col_width) << "terms" << space
     // 6 for remaining parts of scientific notation e.g. "1.e+12"
-                 << space << std::setw(prec+6) << "sum_up"
-                 << space << std::setw(prec+6) << "sum_down"
-                 << space << std::setw(prec+6) << "rel_diff" << std::endl
+                 << std::setw(prec+6) << "sum_up" << space
+                 << std::setw(prec+6) << "sum_down" << space
+                 << std::setw(prec+6) << "rel_diff" << std::endl
     // Set format flags for the following loop.
               << std::right << std::scientific << std::setprecision(prec);
-    for (int n = step; n <= n_max; n += step) {
-        float up = sum_up<float>(n);
-        float down = sum_down<float>(n);
-        float rel_diff = fabs(up-down)/((fabs(up)+fabs(down))/2.0);
+    for (int i = 0, step = 1; i < (int) log10(n_max); ++i, step *= scaling) {
+        for (int n = (int) pow(10, i); n < (int) pow(10, i+1); n += step) {
+            float up = sum_up<float>(n);
+            float down = sum_down<float>(n);
+            float rel_diff = fabs(up-down)/((fabs(up)+fabs(down))/2.0);
 
-        std::cout << std::setw(terms_col_width) << n
-                     << space << up
-                     << space << down
-                     << space << rel_diff << std::endl;
+            std::cout << std::setw(terms_col_width) << n << space << up << space
+                         << down << space << rel_diff << std::endl;
+        }
     }
 
     return 0;
--- homework/1/sum_order.plt
+++ homework/1/sum_order.plt
@@ -5,10 +5,47 @@ if (!exists('plotfile')) {
     plotfile = 'sum_order.pdf'
 }
 
-set terminal pdfcairo noenhanced
+set terminal pdfcairo noenhanced size 6,6
 set output plotfile
 
-set logscale y
+set style line 1 linetype 1 pointtype 7 pointsize 1
+set style line 2 linetype 2 pointtype 7 pointsize 1
+set style line 3 linetype 3 pointtype 7 pointsize 1
+
+array titles[3];
+titles[1] = 'Upward'; titles[2] = 'Downward'; titles[3] = 'Rel. Diff'
+
+f(a, b, x) = a*x + b
+fit [:4] f(a1, b1, x) datafile using (log10($1)):(log10($4)) via a1,b1
+fit [4:] f(a2, b2, x) datafile using (log10($1)):(log10($4)) via a2,b2
+
+set key top left
+set logscale
+set multiplot
+
+set lmargin at screen 0.1
+set rmargin at screen 0.9
+set tmargin at screen 0.9
+set bmargin at screen 0.55
+set title 'Upward vs. Downward Summation of 1/n'
+set ylabel 'Relative Difference'
+plot 1/0 linestyle 1 with points title titles[3], \
+     datafile using 1:4 linestyle 1 notitle with dots, \
+     10**f(a1, b1, log10(x)) title sprintf('%f*x + %f', a1, b1), \
+     10**f(a2, b2, log10(x)) title sprintf('%f*x + %f', a2, b2)
+
+set lmargin at screen 0.1
+set rmargin at screen 0.9
+set tmargin at screen 0.45
+set bmargin at screen 0.1
+set title 'Upward and Downward Summation'
 set xlabel 'Terms'
-set ylabel 'Up/Down-sum Relative Difference'
-plot datafile using 1:4 title columnheader(4)
+set ylabel 'Upward Sum'
+set y2label 'Downward Sum'
+set ytics nomirror
+set y2tics auto
+unset logscale y
+unset logscale y2
+plot for [i=2:3] 1/0 linestyle i with points title titles[i-1], \
+     datafile using 1:2 linestyle 2 notitle axes x1y1 with dots, \
+     ''       using 1:3 linestyle 3 notitle axes x1y2 with dots
