c11bb326182541c227379b906590f0c211e80f77
Nicholas Todoroff <loppy1243@gmail.com>
2019-02-25 03:44:37 -0500

    homework/2: Formatting

--- homework/2/integrate.h
+++ homework/2/integrate.h
@@ -1,9 +1,10 @@
 /* Integrate func using the respective method over the inclusive range [begin,
- * end] on meshsize number of points. A meshsize of 0 returns 0.0.
+ * end] on meshsize number of points.
+ *
+ * A meshsize of 0 returns 0.0. The meshsize requested may not be the exact meshsize used,
+ * depending on the method.
  *
  * func is expected to take a single argument that is a double and return a
  * double */
-template<class F>
-double simpson(double begin, double end, int meshsize, F func);
-template<class F>
-double milne(double begin, double end, int meshsize, F func);
+template<class F> double simpson(double begin, double end, int meshsize, F func);
+template<class F> double   milne(double begin, double end, int meshsize, F func);
