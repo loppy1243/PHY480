910bfa0cc17d71cff08394ac8d787f9581e5c5e8
Nicholas Todoroff <loppy1243@gmail.com>
2019-02-28 23:57:11 -0500

    homework/2: Add updated (and forgotten) config.mk

--- homework/2/config.mk
+++ homework/2/config.mk
@@ -1,4 +1,8 @@
+# Maybe touch these
 export CXX := g++
 export CXXFLAGS := -O3 $(CXXFLAGS)
 export BUILD_PREFIX := $(CURDIR)/build
 export EXE_PREFIX := $(CURDIR)/bin
+
+# Probably don't touch these
+export integrate_LIBS := gsl
